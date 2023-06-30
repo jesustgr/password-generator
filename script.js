af69e389ac703126be3ef23531fb4ef2efbd7789
JT Reyes <jesustgreyes@gmail.com>
2023-06-30 16:30:24 -0700

    add length input

--- script.js
+++ script.js
@@ -15,3 +15,18 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+// Get password length from user input
+function getPasswordLength() {
+  var length = prompt("Enter the length of the password (between 8 and 128 characters):");
+
+  // Validate user input
+  while (length < 8 || length > 128) {
+    alert("Please enter a valid password length between 8 and 128 characters.");
+    length = prompt("Enter the length of the password (between 8 and 128 characters):");
+  }
+
+  return length;
+}
