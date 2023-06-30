12c45745b79a3612957c5833a3189632ec935ec7
JT Reyes <jesustgreyes@gmail.com>
2023-06-30 16:45:20 -0700

    build char set and generate random pasword

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -16,34 +13,66 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+//MY CODE STARTS HERE
 
 // Get password length from user input
 function getPasswordLength() {
   var length = prompt("Enter the length of the password (between 8 and 128 characters):");
 
-  // Validate user input
   while (length < 8 || length > 128) {
     alert("Please enter a valid password length between 8 and 128 characters.");
     length = prompt("Enter the length of the password (between 8 and 128 characters):");
   }
-
+  
   return length;
 }
 
 // Generate password based on user-selected criteria
 function generatePassword() {
   var passwordLength = getPasswordLength();
+  
+  // Get character types from user
   var includeLowercase = confirm("Include lowercase characters? (Press Cancel for 'No')");
   var includeUppercase = confirm("Include uppercase characters? (Press Cancel for 'No')");
   var includeNumeric = confirm("Include numeric characters? (Press Cancel for 'No')");
   var includeSpecial = confirm("Include special characters? (Press Cancel for 'No')");
 
-  // Validate user input
   if (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
     alert("You must select at least one character type.");
     return "";
   }
 
   var password = generateRandomPassword(passwordLength, includeLowercase, includeUppercase, includeNumeric, includeSpecial);
+  return password;
+}
+
+// Generate random password based on user criteria
+function generateRandomPassword(length, includeLowercase, includeUppercase, includeNumeric, includeSpecial) {
+  var charset = "";
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numericChars = "0123456789";
+  var specialChars = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
+
+  // Build character set based on user criteria
+  if (includeLowercase) {
+    charset += lowercaseChars;
+  }
+  if (includeUppercase) {
+    charset += uppercaseChars;
+  }
+  if (includeNumeric) {
+    charset += numericChars;
+  }
+  if (includeSpecial) {
+    charset += specialChars;
+  }
+
+  var password = "";
+  for (var i = 0; i < length; i++) {
+    var randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset[randomIndex];
+  }
+
   return password;
 }
